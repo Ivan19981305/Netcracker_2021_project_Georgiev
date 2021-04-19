9a84311c41524a01cd2f158e12cc2d6f8a70c526
Ivan19981305 <Ivan19981305@yandex.ru>
2021-04-19 05:35:01 +0300

    Initial commit

---
+++ Project_source/src/Network.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Network {
+    HashMap<PathElement, Integer> pathElements;
+
+    public HashMap<PathElement, Integer> getPathElements() {
+        return pathElements;
+    }
+}
